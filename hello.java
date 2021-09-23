066c2434378182090edc5ce7ec185dc3233e1b2c
amythologic1 <71964597+amythologic1@users.noreply.github.com>
2021-09-23 10:33:20 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
   public static void main(String args)
   {
-    System.out.println("this is hello.java file")
+    System.out.println("this is hello.java file");
   
     }
 }
